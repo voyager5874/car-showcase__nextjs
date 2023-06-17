f7882c594e1677872008db9945d559568cd58585
Aleksandr Savkin <voyager5874@gmail.com>
2023-06-17 18:03:41 +0300

    create Hero component

---
+++ components/Button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { MouseEventHandler } from "react";
+import Image from "next/image";
+
+type PropsType = {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+};
+export const Button = ({
+  isDisabled,
+  btnType,
+  containerStyles,
+  textStyles,
+  title,
+  rightIcon,
+  handleClick,
+}: PropsType) => (
+  <button
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+);
